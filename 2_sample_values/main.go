886cff21bc9a3abfa06d59f947bfb8dcc07a1dfd
soumik171 <soumiksarker8@gmail.com>
2025-11-02 12:41:43 +0600

    first commit

---
+++ 2_sample_values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// simple values/int
+	fmt.Println(1 + 1)
+
+	// string
+	fmt.Println("Hello everyone")
+
+	// bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// float
+	fmt.Println(10.6)
+	fmt.Println(4 / 3)     // get decimal values
+	fmt.Println(4.0 / 3.0) // get fractional values
+}
