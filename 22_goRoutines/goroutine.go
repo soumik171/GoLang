3be869991c7f1317b687630e93f9bcfdc62f9788
soumik171 <soumiksarker8@gmail.com>
2025-11-04 17:28:55 +0600

    goroutines basic done

---
+++ 22_goRoutines/goroutine.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// why use goroutine:runs concurrently,lightweight, managed by Go's scheduler
+// runs difficult task as a same time where other languages might fail
+func main() {
+
+	for i := 0; i <= 10; i++ {
+
+		go func() {
+			fmt.Println("doing it", i)
+
+		}()
+
+	}
+
+	// delay the process for goroutines: but this is not reliable, see in channel & waitgroup
+
+	time.Sleep(time.Second * 2)
+
+}
