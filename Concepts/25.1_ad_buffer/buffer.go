9254d44ea8393c94bd9e9fb01392f96ff7c40f9f
soumik171 <soumiksarker8@gmail.com>
2025-11-05 15:35:01 +0600

    channel buffer extention(ad.)

---
+++ Concepts/25.1_ad_buffer/buffer.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	chan1 := make(chan int)
+	chan2 := make(chan string)
+
+	go func() {
+		chan1 <- 10
+	}()
+
+	go func() {
+		chan2 <- "String channel"
+	}()
+
+	// using select to receive multiplechannels:
+	for i := 0; i < 2; i++ {
+		select {
+		case chan1Val := <-chan1:
+			fmt.Println("received data from chan1", chan1Val)
+		case chan2Val := <-chan2:
+			fmt.Println("received data from chan2", chan2Val)
+		}
+	}
+
+}
